62d59fef170b6e5405581cb57de6ee16acedf77c
salman <salmaniit.ahmed1@gmail.com>
2017-08-17 21:41:18 +0530

    Basic implementation of Stack

---
+++ basic/stack.py
@@ -0,0 +1,18 @@
+class Stack():
+	def __init__(self):
+		self.data = []
+
+	def isEmpty(self):
+		return self.data == []
+
+	def push(self,item):
+		self.data.append(item)
+
+	def pop(self):
+		return self.data.pop()
+
+	def peek(self):
+		return self.data[-1]
+
+	def size(self):
+		return len(self.data)
